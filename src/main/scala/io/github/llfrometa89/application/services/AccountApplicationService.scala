19bef041d6e7ac4a6deafa81b6e8a6d64386d254
Livan Frometa <llfrometa@gmail.com>
2019-04-03 09:56:16 +0200

    Add EmailKey

--- src/main/scala/io/github/llfrometa89/application/services/AccountApplicationService.scala
+++ src/main/scala/io/github/llfrometa89/application/services/AccountApplicationService.scala
@@ -5,6 +5,7 @@ import cats.implicits._
 import io.github.llfrometa89.application.converters.AccountConverter
 import io.github.llfrometa89.application.dto.CreateAccount
 import io.github.llfrometa89.application.dto.Transfer
+import io.github.llfrometa89.domain.enums.EmailKey
 import io.github.llfrometa89.domain.model.Savings
 import io.github.llfrometa89.domain.services.AccountService
 import io.github.llfrometa89.domain.utils._
@@ -17,7 +18,7 @@ object AccountApplicationService {
     for {
       accountNo <- G.generate.pure[F]
       account   <- AccountService[F].open(accountNo, req.name, req.email, req.amount, Savings)
-      _         <- mailerWA.send(req.email, Map("name" -> req.name))
+      _         <- mailerWA.send(req.email, Map(EmailKey.NAME -> req.name))
     } yield AccountConverter.toDTO(account)
 
   def transfer[F[_]: Monad: AccountService](req: Transfer.Request)(
@@ -26,7 +27,11 @@ object AccountApplicationService {
     for {
       result <- AccountService[F].transfer(req.senderAccountNo, req.receiverAccountNo, req.amount)
       (senderAccount, receiverAccount) = result
-      _ <- mailerST.send(senderAccount.email, Map("name"   -> senderAccount.name, "amount"   -> req.amount.toString()))
-      _ <- mailerRT.send(receiverAccount.email, Map("name" -> receiverAccount.name, "amount" -> req.amount.toString()))
-    } yield "Success transfer"
+      _ <- mailerST.send(
+        senderAccount.email,
+        Map(EmailKey.NAME -> senderAccount.name, EmailKey.AMOUNT -> req.amount.toString()))
+      _ <- mailerRT.send(
+        receiverAccount.email,
+        Map(EmailKey.NAME -> receiverAccount.name, EmailKey.AMOUNT -> req.amount.toString()))
+    } yield "Success transfer" //TODO Refactoring with BaseResponse
 }
---
+++ src/main/scala/io/github/llfrometa89/domain/enums/EmailKey.scala
@@ -0,0 +1,8 @@
+package io.github.llfrometa89.domain.enums
+
+object EmailKey {
+  type EmailKey = String
+
+  val NAME   = "name"
+  val AMOUNT = "amount"
+}
